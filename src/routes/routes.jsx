0d68e2ec149b78bfae6d1eb0d0f724410ca42f28
JubairBinSayeed <jubairbinsayeed@gmail.com>
2024-12-31 13:33:34 +0600

    starting project

---
+++ src/routes/routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div className="text-center font-bold text-2xl underline mt-96">Hello World!</div>  
+  }
+]);
+export default router
